6f4809f74839216ea27afd778bfe16759c2b559f
Sarkhanas <daniilzon@mail.ru>
2021-12-12 10:51:40 +0300

    footer remade

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,103 +1,104 @@
 import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
 
 export default function FoOter() {
     return (
         <>
-            <footer class="pt-4 my-md-5 pt-md-5 border-top">
-                <div class="row">
-                    <div class="col-12 col-md">
+            <Container>
+                <Row>
+                    <Col md='3'>
                         <h5>Sarkhanas</h5>
                         <ul class="text-unstyled text-small">
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>                                     
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                         </ul>
-                    </div>
-                    <div class="col-6 col-md">
+                    </Col>
+                    <Col md='3'>
                         <h5>Sarkhanas</h5>
                         <ul class="text-unstyled text-small">
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                         </ul>
-                    </div>
-                    <div class="col-6 col-md">
+                    </Col>
+                    <Col md='3'>
                         <h5>Sarkhanas</h5>
                         <ul class="text-unstyled text-small">
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                         </ul>
-                    </div>
-                    <div class="col-6 col-md">
+                    </Col>
+                    <Col md='3'>
                         <h5>Sarkhanas</h5>
                         <ul class="text-unstyled text-small">
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                             <li>
-                                <a href="" class="link-secondary">Advertising</a>
+                                <a href="/" class="link-secondary">Advertising</a>
                             </li>
                         </ul>
-                    </div>
-                </div>
-            </footer>
+                    </Col>
+                </Row>
+            </Container>
         </>)
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, Button } from 'react-bootstrap';
+import { Nav, Navbar, Button, Container, Row, Col } from 'react-bootstrap';
 import {
     BrowserRouter as Router,
     Route,
@@ -13,22 +13,31 @@ export default function NaviBar() {
     return (
         <>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand>Sarkhanas</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/">Articles</Nav.Link>
-                        <Nav.Link href="/news">News</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Button variant="primary" className="mr-2">
-                            Log In
-                        </Button>
-                        <Button variant="primary">Sign Out</Button>
-                    </Nav>
-                </Navbar.Collapse>
+                <Container>
+                    <Navbar.Brand>Sarkhanas</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <Nav className="mr-auto" >
+                                        <Nav.Link href="/">Articles</Nav.Link>
+                                        <Nav.Link href="/news">News</Nav.Link>
+                                    </Nav>
+                                </Col>
+                                <Col me='auto'>
+                                    <Nav className="justify-content-end">
+                                        <Button variant="primary" className="me-md-2">
+                                            Log In
+                                        </Button>
+                                        <Button variant="primary">Sign Out</Button>
+                                    </Nav>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
-
             <Router>
                 <Routes>
                     <Route exact path="/" component={Home} />
